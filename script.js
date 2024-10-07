a21b9b8f24c82e83fa145eda2042429ad9cdb1a7
LiquiNaut <boetfly@gmail.com>
2024-10-07 18:06:31 +0200

    mobile view fix

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ document.oncontextmenu = function () {
     return false;
 }
 
-const countEl = document.getElementById('count');
-
-updateVisitCounter();
-//used CountApi with {"namespace":"s-taxi.sk","key":"views","value":0}
-//info about my key: https://api.countapi.xyz/info/s-taxi.sk/views
-function updateVisitCounter() {
-    fetch('https://api.countapi.xyz/update/s-taxi.sk/views/?amount=1')
-        .then(res => res.json())
-        .then(res => {
-            countEl.innerHTML = res.value;
-        })
-}
+// const countEl = document.getElementById('count');
+//
+// updateVisitCounter();
+// //used CountApi with {"namespace":"s-taxi.sk","key":"views","value":0}
+// //info about my key: https://api.countapi.xyz/info/s-taxi.sk/views
+// function updateVisitCounter() {
+//     fetch('https://api.countapi.xyz/update/s-taxi.sk/views/?amount=1')
+//         .then(res => res.json())
+//         .then(res => {
+//             countEl.innerHTML = res.value;
+//         })
+// }
